87a93f533d3b6376651668e20762596c5fe2ed1a
Jur van Weerden <j.a.vanweerden@students.uu.nl>
2024-03-10 10:58:38 +0100

    book-info (table) html

--- js/info.js
+++ js/info.js
@@ -4,18 +4,62 @@ function generateText() {
     var author;
 
     var newPar = document.createElement("img");
-    newPar.setAttribute("src", "../img/J.R.R.Tolkien.jpg");
+    //newPar.setAttribute("src", "../img/J.R.R.Tolkien.jpg");
     const aside = document.getElementsByTagName("aside")[0];
     aside.appendChild(newPar);
     const test = document.getElementById("test_p");
     
     createInstances();
     test.innerText = book.getTitle;
+
+    createBookElements(book);
+
+    function createBookElements(book) {
+        //const article = document.getElementById("book-article");
+        table = document.getElementById("book-info");
+
+        //const title = document.createElement("h3");
+        //title.innerText = book.getTitle;
+
+        //const coverImage = document.createElement("img");
+        //coverImage.setAttribute("src", book.getCover);
+
+        //const infoSection = document.createElement("section");
+
+        var rows = [];
+        for (var i = 0; i < 6; i++) {rows[i] = document.createElement("tr")}
+
+        var cells = [];
+        for (var i = 0; i < 5; i++) {cells[i] = document.createElement("td")}
+
+        const header = document.createElement("th");
+
+        header.innerText = book.getTitle;
+
+        const coverImage = document.createElement("img");
+        coverImage.setAttribute("src", book.getCover);
+        cells[0].appendChild(coverImage);
+
+        cells[1].innerText = "Author(s): " + book.getAuthors[0];
+        cells[2].innerText = "Year of creation: " + book.getCreationYear;
+        cells[3].innerText = "Genre: " + book.getGenre;
+        cells[4].innerText = "Plot: " + book.getPlot;
+
+        rows[0].appendChild(header);
+        for (var i = 0; i < cells.length; i++) {
+            rows[i + 1].appendChild(cells[i]);
+        }
+
+        for (var i = 0; i < rows.length; i++) {table.appendChild(rows[i])}
+
+        //article.appendChild(title);
+        //article.appendChild(coverImage);
+    }
     
 
     function createInstances() {
         const plot = '"The Lord of the Rings: The Return of the King" concludes J.R.R. Tolkien\'s epic fantasy trilogy with the final chapter of the War of the Ring. The story follows the efforts of the Fellowship as they strive to defeat the dark forces of Sauron and bring peace to Middle-earth. As the fate of the realm hangs in the balance, heroes emerge and sacrifices are made in a climactic battle between good and evil.';
-        book = new Book("J.R.R Tolkien", 1955, "Lord of the Rings: Return of the King", "Fantasy", "https://upload.wikimedia.org/wikipedia/en/thumb/1/11/The_Return_of_the_King_cover.gif/220px-The_Return_of_the_King_cover.gif", plot);
+        book = new Book(["J.R.R Tolkien"], 1955, "Lord of the Rings: Return of the King", "Fantasy", "https://upload.wikimedia.org/wikipedia/en/thumb/1/11/The_Return_of_the_King_cover.gif/220px-The_Return_of_the_King_cover.gif", plot);
         
         const publisherBookList = [
             "The Lord of Rings: Return of the King",
