20e83e563ddd947a87f83891087d849780cfa26c
kdkoerts <kdkoerts@users.noreply.github.com>
2024-03-15 15:06:42 +0100

    purple

--- js/summary.js
+++ js/summary.js
@@ -1,8 +1,51 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    var test = document.getElementById("spoiler-summary").offsetHeight + document.getElementById("spoiler-summary").offsetTop;
-    var test2 = document.getElementById("spoiler-free-summary").offsetHeight + document.getElementById("spoiler-free-summary").offsetTop;
-    console.log("spoiler (rel): " + test + "nospoiler (static): " + test2);
-    var foot = document.getElementsByTagName("footer");
-    foot.top = test;
-})
+    var selectElement = document.getElementById("select-element");
+    const body = document.body;
+    var children = body.children;
+    function buildSelect(){
+        var bodyOption = document.createElement('option');
+        bodyOption.value = bodyOption.innerHTML = body.tagName;
+        selectElement.appendChild(bodyOption);
+        for (var i = 0; i < children.length; i++) {
+            var child = children[i];
+            var newOption = document.createElement('option');
+            newOption.value = child.tagName;
+            newOption.innerHTML = child.tagName;
+            selectElement.appendChild(newOption);
+        }
+    }
+    buildSelect();
+    const button = document.getElementById('change-element');
+    button.addEventListener('click', function () {
+        const selectedIndex = selectElement.selectedIndex;
+        const selectChange = document.getElementById('select-change');
+        const selectChangeIndex = selectChange.selectedIndex;
+        switch (selectChangeIndex) {
+            case 0:
+                if (selectedIndex > 0) {
+                    children[selectedIndex-1].style.color = "blue";
+                }else{
+                    body.style.color = "blue";
+                }
+                break;
+            case 1:
+                if (selectedIndex > 0) {
+                    children[selectedIndex-1].style.backgroundColor = "purple";
+                }else{
+                    body.style.backgroundColor = "purple";
+                }
+                break;
+            case 2:
+                if (selectedIndex > 0) {
+                    children[selectedIndex-1].style.fontSize = "2.0em";
+                }else{
+                    body.style.fontSize = "2.0em";
+                }
+                break;
+            default:
+                break;
+        }
+        
+    });
+});
