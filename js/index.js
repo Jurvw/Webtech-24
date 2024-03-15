c4432c8c92a760dbf81564f198b31a38c964f7ba
Jur van Weerden <j.a.vanweerden@students.uu.nl>
2024-03-15 16:30:48 +0100

    index comments

--- js/index.js
+++ js/index.js
@@ -1,26 +1,33 @@
+//set bools to be able to switch between states
 var imgSize = false;
 var coverVersion = false;
 
 document.addEventListener('DOMContentLoaded', function() {
-    var zoomDiv = document.getElementById("book-cover");
+    //get the relevant elements
+    var scrollDiv = document.getElementsByTagName("aside")[0];
     var coverImage = document.getElementById("book-cover__image");
 
-    zoomDiv.addEventListener("click", zoomImg, false);
+    //add the eventlistener to (un)zoom the image
+    coverImage.addEventListener("click", zoomImg, false);
 
-    var scrollDiv = document.getElementsByTagName("aside")[0];
+    //add the evenlistener to switch between different versions of the cover
     scrollDiv.addEventListener("click", scrollBookImg, false);
 
     function scrollBookImg(event) {
+        //changes the source of the image to a different one if the state is changed by clicking
         if (coverVersion) {
             coverImage.src = "../img/lotr_return_of_the_king_cover.jpg";
         }
         else {
             coverImage.src = "https://m.media-amazon.com/images/I/41KPaTTRxFL.jpg";
         }
+
+        //update the current state
         coverVersion = !coverVersion;
     }
 
     function zoomImg(event) {
+        //changes the size of the image depending on it's current state
         if (imgSize) {
             event.target.width /= 2;
             event.target.height /= 2;
@@ -30,8 +37,10 @@ document.addEventListener('DOMContentLoaded', function() {
             event.target.height *= 2;
         }
 
+        //update the current state
         imgSize = !imgSize;
         
+        //cancel the switching of the source of the image when both are called
         event.stopPropagation();
     }
 });
