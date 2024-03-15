1963e86f37a41a58b9d9e111d6f565f9f004bb5d
kdkoerts <kdkoerts@users.noreply.github.com>
2024-03-15 16:09:37 +0100

    comments to footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,12 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    //Get the selectbox from the DOM
     var selectElement = document.getElementById("select-element");
+    //get body from DOM and make it a const for further use
     const body = document.body;
+    //All the children of body in DOM
     var children = body.children;
+    //This function fills the selectbox named select-element with the body and children of body according to DOM
     function buildSelect(){
+        //Makes an option for a selectbox
         var bodyOption = document.createElement('option');
+        // Fills the option with body
         bodyOption.value = bodyOption.innerHTML = body.tagName;
+        //Adds the body option to the select box
         selectElement.appendChild(bodyOption);
+        //In this loop we take the ith child and add its name as value and text as an option to the select box.
         for (var i = 0; i < children.length; i++) {
             var child = children[i];
             var newOption = document.createElement('option');
@@ -18,14 +26,18 @@ document.addEventListener('DOMContentLoaded', function() {
     buildSelect();
     const button = document.getElementById('change-element');
     button.addEventListener('click', function () {
+        // This const holds the index of the select-element selectbox
         const selectedIndex = selectElement.selectedIndex;
+        //This does the same as the const above but for the select-change selectbox
         const selectChange = document.getElementById('select-change');
         const selectChangeIndex = selectChange.selectedIndex;
+        //We switch on the index of the second selectbox to change element selected in the first selectbox by the value associated with the second selectbox
         switch (selectChangeIndex) {
             case 0:
+                // If selectIndex is > 0 that means it is a child and we take the value of that child to the chosen style.
                 if (selectedIndex > 0) {
                     children[selectedIndex-1].style.color = "blue";
-                }else{
+                }else{ //This means the body is selected and will be changed
                     body.style.color = "blue";
                 }
                 break;
