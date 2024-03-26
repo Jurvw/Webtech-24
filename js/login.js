761dd5d4ed73b3e1f685cef8e039c7d91e5bd7db
Jur van Weerden <j.a.vanweerden@students.uu.nl>
2024-03-26 16:09:52 +0100

    login testing on server

--- js/login.js
+++ js/login.js
@@ -1,13 +1,37 @@
+
+
 function submitCredentials() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    alert(username + " " + password);
 
-    var cookieInhoud = "username=" + username + "; password=" + password;
+    document.cookie = "username=" + username + ";max-age=10;secure;path=/"
+    document.cookie = "password=" + password + ";max-age=10;secure;path=/"
+    //document.cookie += "max-age=100;path=/";
 
-    document.cookie = "name=Credentials; " + cookieInhoud;
     //document.cookie = "username=" + username;
-    //document.cookie = "password=" + password;
-    var cookies = document.cookie.split(";");
-    alert(cookies);
+    //document.cookie += ";password=" + password;
+    //document.cookie += ";expires"
+
+    cookies = document.cookie.split(";");
+    alert(cookies[0]);
+
+    const sqlite3 = require('sqlite3').verbose();
+    let db = new sqlite3.Database('credentials.db', sqlite3.OPEN_READONLY);
+
+    db.serialize(() => {
+        db.each(`SELECT user_id as id FROM credentials`, (err, row) => {
+            if (err) {
+                alert(err.message);
+            }
+            else {
+                alert(row.id);
+            }
+        })
+    });
+
+    db.close();
+}
+
+function checkCookies() {
+    alert(document.cookie);
 }
